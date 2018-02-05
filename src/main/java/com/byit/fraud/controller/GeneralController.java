a7ee852bbe83d30b91d86d010d835e9752ece05e
chenzhantao <1103789628@qq.com>
2018-02-05 10:46:19 +0800

    chenzhantao: add method

--- src/main/java/com/byit/fraud/controller/GeneralController.java
+++ src/main/java/com/byit/fraud/controller/GeneralController.java
@@ -1,6 +1,7 @@
 package com.byit.fraud.controller;
 
 import com.byit.fraud.dto.*;
+import com.byit.fraud.beantoxml.*;
 import com.byit.fraud.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -33,5 +35,21 @@ public class GeneralController {
         response.setMessage("success");
         response.setBody(summaries);
         return response;
+    }
+
+
+    /**
+     * chenzht
+     *响应报文返回xml格式
+     */
+    @RequestMapping(value = "/javaToXml",method = RequestMethod.POST)
+    @ResponseBody
+    public BHAFResp XmlToJavaBean(){
+        System.out.println("测试来了");
+        CommonHeader header = new CommonHeader("747474747","asdfsd","hello msg","a32423423","2321123","ewr324234","213234234","234234234","234234");
+        ApplicationInfos applicationInfos = new ApplicationInfos("wq32ed","23dddd","234fdfdfd","23fdd",new Date(),"45fd","sdf23223","2134dsfsd","12dsfsd","123ds","123dsfr3","123er213");
+        BizBody body = new BizBody(applicationInfos);
+        BHAFResp bhafResp = new BHAFResp(header,body);
+        return bhafResp;
     }
 }
